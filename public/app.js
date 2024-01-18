1053851b6de8d987deb157b8c32733faf249e5f0
Md-Asikur <asikurrahaman997@gamil.com>
2024-01-18 20:31:22 +0600

    firstg

---
+++ public/app.js
@@ -0,0 +1,246 @@
+const videoGrid = document.getElementById("video_grid");
+const muteBtn = document.getElementById("muteBtn");
+const cameraoff = document.getElementById("cameraoff");
+const selectCam = document.getElementById("selectCam");
+const selectMic = document.getElementById("selectMic");
+const screenShare = document.getElementById("screenShare");
+
+// socket init
+const socket = io();
+
+let mediaStream;
+let mute = false;
+let camera = true;
+let currentCam;
+let RTC;
+
+// sound mute handler
+muteBtn.addEventListener("click", (e) => {
+  if (mute) {
+    mute = false;
+    muteBtn.textContent = "Mute yourself";
+    mediaStream.getAudioTracks().forEach((track) => {
+      track.enabled = true;
+    });
+  } else {
+    mute = true;
+    muteBtn.textContent = "Unmute yourself";
+    mediaStream.getAudioTracks().forEach((track) => {
+      track.enabled = false;
+    });
+  }
+});
+
+cameraoff.addEventListener("click", () => {
+  if (camera) {
+    cameraoff.textContent = "Turn on camera";
+    camera = false;
+    mediaStream.getVideoTracks().forEach((track) => {
+      track.enabled = false;
+    });
+  } else {
+    cameraoff.textContent = "Turn off camera";
+    camera = true;
+    mediaStream.getVideoTracks().forEach((track) => {
+      track.enabled = true;
+    });
+  }
+});
+
+// getting the medias
+async function getMedia(cameraId, micId) {
+  currentCam = cameraId === null ? currentCam : cameraId;
+
+  const initialConstraits = {
+    video: true,
+    audio: true,
+  };
+
+  const preferredCameraConstraints = {
+    video: {
+      deviceId: cameraId,
+    },
+    audio: true,
+  };
+
+  const videoOption = currentCam
+    ? {
+        deviceId: currentCam,
+      }
+    : true;
+
+  const preferredMicConstraints = {
+    video: videoOption,
+    audio: {
+      deviceId: micId,
+    },
+  };
+
+  try {
+    mediaStream = await window.navigator.mediaDevices.getUserMedia(
+      cameraId || micId
+        ? cameraId
+          ? preferredCameraConstraints
+          : preferredMicConstraints
+        : initialConstraits
+    );
+    // send joining notification
+
+    displayMedia();
+    getAllCameras();
+    getAllMics();
+    makeWebRTCConnection();
+
+    // room joining event
+    socket.emit("joinRoom", roomId);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getMedia();
+
+async function getScreenMedia() {
+  try {
+    mediaStream = await navigator.mediaDevices.getDisplayMedia({
+      audio: true,
+      video: true,
+    });
+    displayMedia();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+screenShare.addEventListener("click", getScreenMedia);
+
+// display media
+function displayMedia() {
+  const video = document.createElement("video");
+  video.srcObject = mediaStream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.appendChild(video);
+}
+
+// get all cameras
+async function getAllCameras() {
+  const currentCamera = mediaStream.getVideoTracks()[0];
+  const allDevices = await window.navigator.mediaDevices.enumerateDevices();
+  selectCam.innerHTML = "";
+  allDevices.forEach((device) => {
+    if (device.kind === "videoinput") {
+      const option = document.createElement("option");
+      option.value = device.deviceId;
+      option.textContent = device.label;
+      option.selected = device.label === currentCamera.label ? true : false;
+      selectCam.appendChild(option);
+    }
+  });
+}
+
+// get all mics
+async function getAllMics() {
+  const currentMic = mediaStream.getAudioTracks()[0];
+  const allDevices = await window.navigator.mediaDevices.enumerateDevices();
+  selectMic.innerHTML = "";
+  allDevices.forEach((device) => {
+    if (device.kind === "audioinput") {
+      const option = document.createElement("option");
+      option.value = device.deviceId;
+      option.textContent = device.label;
+      option.selected = device.label === currentMic.label ? true : false;
+      selectMic.appendChild(option);
+    }
+  });
+}
+
+// select a specific camera
+selectCam.addEventListener("input", (e) => {
+  const cameraId = e.target.value;
+  getMedia(cameraId);
+});
+
+// select a specific camera
+selectMic.addEventListener("input", (e) => {
+  const micId = e.target.value;
+  getMedia(null, micId);
+});
+
+/// socket
+
+socket.on("newJoining", () => {
+  makeAOffer();
+});
+
+// make WebRTC connection
+function makeWebRTCConnection() {
+  // rtc init
+  RTC = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: "stun:stun1.l.google.com:19302",
+      },
+      {
+        urls: "stun:stun3.l.google.com:19302",
+      },
+      {
+        urls: "stun:stun4.l.google.com:19302",
+      },
+    ],
+  });
+
+  // add media tracks to RTC
+  mediaStream.getTracks().forEach((track) => {
+    RTC.addTrack(track, mediaStream);
+  });
+
+  // send ICE candidate
+  RTC.addEventListener("icecandidate", (data) => {
+    socket.emit("sendIceCandidate", data.candidate, roomId);
+  });
+
+  // send ICE candidate
+  RTC.addEventListener("addstream", (data) => {
+    const videoTag = document.createElement("video");
+    videoTag.srcObject = data.stream;
+    videoTag.addEventListener("loadedmetadata", () => {
+      videoTag.play();
+    });
+
+    videoGrid.appendChild(videoTag);
+  });
+}
+
+// make a offer
+async function makeAOffer() {
+  const offer = await RTC.createOffer();
+  RTC.setLocalDescription(offer);
+  // send the offer
+  socket.emit("sendTheOffer", offer, roomId);
+}
+
+// receive offer
+socket.on("receiveOffer", async (offer) => {
+  RTC.setRemoteDescription(offer);
+  const answer = await RTC.createAnswer();
+  RTC.setLocalDescription(answer);
+
+  // send the answer
+  socket.emit("sendTheAnswer", answer, roomId);
+});
+
+// receive answer
+socket.on("receiveAnswer", (answer) => {
+  RTC.setRemoteDescription(answer);
+});
+
+// receive answer
+socket.on("receiveCandidate", (candidate) => {
+  RTC.addIceCandidate(candidate);
+});
+
+/* 
+    1. RTC connection initialization after media stream ready!
+    2. add media tracks to RTC
+*/
